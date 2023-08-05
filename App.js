842ffc4e671450d6d2d2a8f4f17a7e99fd850459
Jeel Patel <34994081+JeelPatel2000@users.noreply.github.com>
2023-08-05 17:16:57 +0800

    test - bg image

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, View, Dimensions } from 'react-native';
+const image = require('./assets/bg.png');
+
+const { width, height } = Dimensions.get('window');
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={image} style={styles.image}/> 
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +20,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: width, // Set your desired width
+    height: height, // Set your desired height
+    resizeMode: 'contain', // Adjust the image's size to fit the container
   },
 });
